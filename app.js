18a6d966e01fd7b0a914e6f6c8483efe90f509e7
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-21 00:01:24 +0500

    done

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+let date = new Date()
+let yearChanger = document.querySelector("#yearChanger")
+yearChanger.innerText = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`
+
 function generate(){
     let input1 = document.querySelector("#input1")
     let input2 = document.querySelector("#input2")
@@ -6,6 +11,8 @@ function generate(){
     for (let i = 1; i<=input2.value; i++) {
         table.innerText = table.innerText + `${input1.value} x ${i} = ${input1.value*i} \n`;}
 
+        input1.value = "";
+        input2.value = "";
     // table.innerText = input1.value
     // table.innerText = input2.value
 
