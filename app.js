914760892c31505839456e854436c7e7913ea376
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-07-07 20:36:04 +0500

    done

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ function generate(){
     let input1 = document.querySelector("#input1")
     let input2 = document.querySelector("#input2")
     let table = document.querySelector("#table")
+    let error = document.querySelector("#error");
+
+    if(input1 === ""){
+        error.innerText = "input1 is required"
+    }
     
     for (let i = 1; i<=input2.value; i++) {
-        table.innerText = table.innerText + `${input1.value} x ${i} = ${input1.value*i} \n`;}
+        table.innerText += `${input1.value} x ${i} = ${input1.value*i} \n`;}
 
         input1.value = "";
         input2.value = "";
-    // table.innerText = input1.value
-    // table.innerText = input2.value
-
 }
